3d64a0d9613c0a6285a0bac6b1281c97601a5ac2
SimonThrane <minijordan23-virum@hotmail.com>
2016-03-16 11:45:10 +0100

    Billeder

--- Musicbox.m
+++ Musicbox.m
@@ -30,37 +30,33 @@ W = fft(x_hann,N);
 
 % ***** Udklat et signal ***************************************
 %Smoothen = raw_smooth(X, N-1);
-[f_oct3, Xm] = oct_smooth(X, Fsample, 12, [1 40000]);
-%S_female_smooth = raw_smooth(W, 31);
-%S_female_smoothdB = 20*log10(abs(S_female_smooth));
-
+[f_oct3, Xm] = oct_smooth(X, Fsample, 12, [1 22000]);
 
 Music_oct12dB = 20*log10(abs((2/N)*Xm));
 
 % ***** Plot ***************************************************
 figure(1); clf
-subplot(2,2,1);
-semilogx(20*log10(abs((2/N)*X(1:0.5*(end-1)))))
-xlabel('Samplebins')
+semilogx(f_axis(1:0.5*end),20*log10(abs((2/N)*X(1:0.5*(end)))))
+xlabel('Frekvens i Herz')
 ylabel('Størrelse i dB ift. 1 Volt')
 title('Musikbox DFT-signal')
 grid on
 
-subplot(2,2,2);
-semilogx(20*log10(abs((2/N)*W(1:0.5*(end-1)))))
-xlabel('Samplebins')
+figure(2); clf
+semilogx(f_axis(1:0.5*end),20*log10(abs((2/N)*W(1:0.5*(end)))))
+xlabel('Frekvens i Herz')
 ylabel('Størrelse i dB ift. 1 Volt')
 title('Musikbox med Hanning vindue')
 grid on
 
-subplot(2,2,3);
+figure(3); clf
 semilogx(f_oct3, Music_oct12dB)
-xlabel('Frekvens')
-ylabel('Styrke')
+xlabel('Frekvens i Herz')
+ylabel('Størrelse i dB ift. 1 Volt')
 title('Udglattet signal')
 grid on
 
-subplot(2,2,4);
+figure(4); clf
 plot(x)
 xlabel('Samples')
 ylabel('Amplitude (volt)')
